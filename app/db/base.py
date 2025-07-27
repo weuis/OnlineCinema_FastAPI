905dd164d83481ce04afe68f440dcc332adc714a
Алексей Розеган <rozeganalexey@gmail.com>
2025-07-27 15:56:15 +0200

    First settings for future db

--- app/db/base.py
+++ app/db/base.py
@@ -1,10 +1,5 @@
-from app.models.user import User
-from app.models.user_group import UserGroup
-from app.models.tokens import ActivationToken, PasswordResetToken, RefreshToken
-from app.models.user_profile import UserProfile
-
-
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
